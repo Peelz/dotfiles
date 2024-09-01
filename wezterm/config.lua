70585fb4463674860032967cf2be6a72e454f547
Anucha Mai <anucha.mai@truedigital.com>
2024-09-01 14:19:12 +0700

    update font config

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -17,31 +17,47 @@ config.color_scheme = "Tokyo Night Storm"
 
 config.window_background_opacity = 0.85
 
-config.font = wezterm.font("Jetbrain Mono", { weight = "Medium" })
-config.font_size = 9
+-- config.font = wezterm.font("Jetbrain Mono", { weight = "Medium" })
+--
+config.font = wezterm.font_with_fallback({
+	-- <built-in>, BuiltIn
+	-- AKA: "JetBrains Mono ExtraLight"
+	{ family = "JetBrains Mono", weight = "Regular", style = "Normal" },
 
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
+	-- <built-in>, BuiltIn
+	"JetBrains Mono",
+
+	-- <built-in>, BuiltIn
+	-- Assumed to have Emoji Presentation
+	"Noto Color Emoji",
+
+	-- <built-in>, BuiltIn
+	"Symbols Nerd Font Mono",
+})
+config.font_size = 10
+
+-- wezterm.on("user-var-changed", function(window, pane, name, value)
+-- 	local overrides = window:get_config_overrides() or {}
+-- 	if name == "ZEN_MODE" then
+-- 		local incremental = value:find("+")
+-- 		local number_value = tonumber(value)
+-- 		if incremental ~= nil then
+-- 			while number_value > 0 do
+-- 				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+-- 				number_value = number_value - 1
+-- 			end
+-- 			overrides.enable_tab_bar = false
+-- 		elseif number_value < 0 then
+-- 			window:perform_action(wezterm.action.ResetFontSize, pane)
+-- 			overrides.font_size = nil
+-- 			overrides.enable_tab_bar = true
+-- 		else
+-- 			overrides.font_size = number_value
+-- 			overrides.enable_tab_bar = false
+-- 		end
+-- 	end
+-- 	window:set_config_overrides(overrides)
+-- end)
 
 -- and finally, return the configuration to wezterm
 return config
