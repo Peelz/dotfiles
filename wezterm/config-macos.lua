153326f7ff3ba5a805240d323de3feefcc2e686c
Anucha Mai <anucha.mai@truedigital.com>
2025-09-06 00:20:39 +0700

    just update

--- wezterm/config-macos.lua
+++ wezterm/config-macos.lua
@@ -1,27 +1,119 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- This table will hold the configuration.
 local config = {}
 
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night Storm"
 
 config.window_background_opacity = 0.9
 
+-- config.font = wezterm.font("Jetbrain Mono", { weight = "Medium" })
+--
 config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	-- <built-in>, BuiltIn
+	-- AKA: "JetBrains Mono ExtraLight"
 	{ family = "JetBrains Mono", weight = "Regular", style = "Normal" },
+
+	-- <built-in>, BuiltIn
+	"JetBrains Mono",
+
+	-- <built-in>, BuiltIn
+	-- Assumed to have Emoji Presentation
 	"Noto Color Emoji",
+
+	-- <built-in>, BuiltIn
 	"Symbols Nerd Font Mono",
 })
-config.font_size = 14
+config.font_size = 12
+
+-- wezterm.on("user-var-changed", function(window, pane, name, value)
+-- 	local overrides = window:get_config_overrides() or {}
+-- 	if name == "ZEN_MODE" then
+-- 		local incremental = value:find("+")
+-- 		local number_value = tonumber(value)
+-- 		if incremental ~= nil then
+-- 			while number_value > 0 do
+-- 				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+-- 				number_value = number_value - 1
+-- 			end
+-- 			overrides.enable_tab_bar = false
+-- 		elseif number_value < 0 then
+-- 			window:perform_action(wezterm.action.ResetFontSize, pane)
+-- 			overrides.font_size = nil
+-- 			overrides.enable_tab_bar = true
+-- 		else
+-- 			overrides.font_size = number_value
+-- 			overrides.enable_tab_bar = false
+-- 		end
+-- 	end
+-- 	window:set_config_overrides(overrides)
+-- end)
+
+-- and finally, return the configuration to wezterm
+--
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 config.keys = {
+	-- ...
 	{
-		key = "Enter",
-		mods = "LEADER",
-		action = wezterm.action.ActivateCopyMode,
+		key = "w",
+		mods = "ALT",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+		end),
+	},
+	{
+		key = "W",
+		mods = "ALT",
+		action = resurrect.window_state.save_window_action(),
+	},
+	{
+		key = "T",
+		mods = "ALT",
+		action = resurrect.tab_state.save_tab_action(),
+	},
+	{
+		key = "s",
+		mods = "ALT",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+			resurrect.window_state.save_window_action()
+		end),
+	},
+	{
+		key = "r",
+		mods = "ALT",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				if type == "workspace" then
+					local state = resurrect.state_manager.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.state_manager.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.state_manager.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end)
+		end),
 	},
 }
 
